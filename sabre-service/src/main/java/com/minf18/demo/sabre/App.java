f0a26c685e65c33ed1ab7f94593b1a6bdbd12a20
quang nhat <tranquangnhat2211@gmail.com>
2020-09-20 21:37:26 +0700

    Initial project

---
+++ sabre-service/src/main/java/com/minf18/demo/sabre/App.java
@@ -0,0 +1,13 @@
+package com.minf18.demo.sabre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
